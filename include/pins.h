9e9b994c8ed3a7a80033bc37d4a11fdfeff5a2fb
Jan Machacek <jan.machacek@gmail.com>
2019-12-15 12:53:14 +0000

    x

--- include/pins.h
+++ include/pins.h
@@ -1,4 +1,4 @@
 #define LED_STRIP_PIN 13
-#define PIR_SENSOR_PIN 12
+#define PIR_SENSOR_PIN 2
 
 #define LIGHT_SENSOR_PIN A0
--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,42 @@
 #include <FastLED.h>
 #include "pins.h"
 
-static const int LED_COUNT = 8;
+static const int maxBrightness = 100;
+static const int peeDelay = 2000;  // 1000 * 60 * 10;
+static bool lightUp = false;
+static const int LED_COUNT = 28;
 static CRGB leds[LED_COUNT];
+
+void setColour() {
+  for (int i = 0; i < LED_COUNT; i++) leds[i] = CRGB(0x00ff00);
+}
+
+void irDetected() {
+  lightUp = true;
+  Serial.println("Intr");
+}
 
 void setup() {
   randomSeed(analogRead(0));
   Serial.begin(115200);
-
   FastLED.addLeds<NEOPIXEL, LED_STRIP_PIN>(leds, LED_COUNT);
-  FastLED.setBrightness(20);
+  attachInterrupt(digitalPinToInterrupt(PIR_SENSOR_PIN), irDetected, RISING);
 }
 
 void loop() {
-  for (int i = 0; i < LED_COUNT; i++) {
-    leds[i] = CRGB(0x00ff00);
-    FastLED.show();
-    delay(200);
+  if (lightUp) {
+    setColour();
+    for (int i = 0; i < maxBrightness; i++) {
+      FastLED.setBrightness(i);
+      FastLED.show();
+      delay(50);
+    }
+    delay(peeDelay);
+    for (int i = maxBrightness; i >= 0; i--) {
+      FastLED.setBrightness(i);
+      FastLED.show();
+      delay(50);
+    }
+    lightUp = false;
   }
-
-  FastLED.show();
 }
